2cbb23dcaa81d5318d38f698bed08e2e2c2394e9
Holllmess <spasskih221@gmail.com>
2022-02-20 10:47:13 +0300

    Add void PrintStringArray

--- MainTask/Program.cs
+++ MainTask/Program.cs
@@ -2,3 +2,21 @@
 // строк, длина которых меньше либо равна 3 символа. Первоначальный массив можно 
 // ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не 
 // рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+using static System.Console;
+
+string[] stringArray = { "hello", "world", "2", ":-)" };
+
+void PrintStringArray(string[] stringArray)
+{
+  Write("["); // 6-я строка убирает console 
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    Write($"\"{stringArray[i]}\"");
+    if (i != stringArray.Length - 1)
+    {
+      Write(", ");
+    }
+  }
+  Write("]");
+}
